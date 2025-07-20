a2cee02ae57ddf587b9701c4e3bbbba9c961c5ef
Trevor DePew <t.depewmusic@gmail.com>
2025-07-20 17:14:18 -0400

    initial commit

---
+++ src/main/java/com/trevor/portfolio/mockapi/MockApiApplication.java
@@ -0,0 +1,13 @@
+package com.trevor.portfolio.mockapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockApiApplication.class, args);
+	}
+
+}
